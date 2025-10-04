67b3b56b1d717b9b02280935dfd80b18f594e0c5
MarcYoba <yobamarc1@gmail.com>
2025-10-04 15:40:37 +0100

    creation de la fonction de telecharger du bilan du logiciel

--- src/Controller/PassifController.php
+++ src/Controller/PassifController.php
@@ -11,6 +11,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 
 class PassifController extends AbstractController
 {
@@ -206,5 +208,47 @@ class PassifController extends AbstractController
             }
         }
         return $this->json(['error' => 'Erreur de bilan'], 404);
+    }
+
+    #[Route('/passif/download', name: 'passif_download')]
+    public function download(EntityManagerInterface $em, Request $request) : Response 
+    {
+        $date = date("Y-m-d");
+        if ($request->isMethod('POST')) {
+           $date = $request->request->get('date');
+           if(empty($date))
+           {
+                $this->addFlash("error", "Vous deviez selectiion aune date valide");
+                return $this->redirectToRoute("app_actif_list");
+           }
+        }
+
+        $options = new Options();
+        $options->set('isRemoteEnabled', true); // Permet les assets distants (CSS/images)
+        $dompdf = new Dompdf($options);
+        $passif = $em->getRepository(Passif::class)->findByYear($date);
+        
+        
+        //dd($vente);
+        $html = $this->renderView('passif/dwonload.html.twig', [
+        'passifs' => $passif,
+        'date' => $date,
+        ]);
+
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A4', 'portrait');
+
+        // 5. Rendre le PDF
+        $dompdf->render();
+
+        // 6. Retourner le PDF dans la réponse
+        return new Response(
+            $dompdf->output(),
+            Response::HTTP_OK,
+            [
+                'Content-Type' => 'application/pdf',
+                'Content-Disposition' => 'inline; filename="document.pdf"', // 'inline' pour affichage navigateur
+            ]
+        );
     }
 }
