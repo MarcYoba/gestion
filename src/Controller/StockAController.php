0b7dbe2ddea33ba900863e2e94ba0a81bd12021d
MarcYoba <yobamarc1@gmail.com>
2026-02-03 20:58:07 +0100

    creation du recapitulatif des stock

--- src/Controller/StockAController.php
+++ src/Controller/StockAController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\AchatA;
+use App\Entity\FactureA;
 use App\Entity\HistoriqueA;
+use App\Entity\MagasinA;
 use App\Entity\ProduitA;
 use App\Entity\TempAgence;
 use Doctrine\ORM\EntityManagerInterface;
@@ -17,7 +19,7 @@ class StockAController extends AbstractController
     public function index(EntityManagerInterface $em): Response
     {
         $user = $this->getUser();
-        $date = date("Y"."01"."04");
+        $date = date("Y"."-01"."-04");
         
         $produits = [];
         $tempagence = $em->getRepository(TempAgence::class)->findOneBy(['user' => $user]);
@@ -26,13 +28,19 @@ class StockAController extends AbstractController
         foreach ($produit as $key => $value) {
             $historiquequatite = 0;
             $historiques = $em->getRepository(HistoriqueA::class)->findByProduitAgence($value,$id,$date);
+            $sommeachat = $em->getRepository(AchatA::class)->findBySommeAchatProduitDay($date,$value,$id);
+            $sommeventejour = $em->getRepository(FactureA::class)->findByQuantiteProduitVendu(new \DateTimeImmutable(), $value->getid(), $id);
+            $sommevente = $em->getRepository(FactureA::class)->findByQuantiteProduitVenduAnne($date, $value, $id);
+            $magasinQt = $em->getRepository(MagasinA::class)->findOneBy(['produit' => $value, 'agence' => $id]);
             if ($historiques) {
                 $historiquequatite = $historiques->getQuantite();
             }
-            array_push($produits, $value,$historiquequatite);
+            array_push($produits, [$value->getNom(),$historiquequatite, $sommeachat,$sommeventejour,$sommevente,$value->getQuantite(),empty($magasinQt)?0:$magasinQt->getQuantite()]);
         }
+        
         return $this->render('stock_a/recapitulatif.html.twig', [
             'produits' => $produit,
+            'produitsData' => $produits,
 
         ]);
     }
