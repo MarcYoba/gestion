4217e614c426e527232bfca26e113a761f66d514
MarcYoba <yobamarc1@gmail.com>
2026-02-03 22:39:11 +0100

    calcule des perte de l'entreprise

--- src/Controller/StockAController.php
+++ src/Controller/StockAController.php
@@ -44,4 +44,35 @@ class StockAController extends AbstractController
 
         ]);
     }
+
+    #[Route('/stock/a/perte', name: 'app_stock_a_perte')]
+    public function perte(EntityManagerInterface $em): Response
+    {
+        $user = $this->getUser();
+        $date = date("Y"."-01"."-04");
+        
+        $produits = [];
+        $tempagence = $em->getRepository(TempAgence::class)->findOneBy(['user' => $user]);
+        $id = $tempagence->getAgence()->getId();
+        $produit = $em->getRepository(ProduitA::class)->findBy(["agence" => $id]);
+        foreach ($produit as $key => $value) {
+            $historiquequatite = 0;
+            $historiques = $em->getRepository(HistoriqueA::class)->findByProduitAgence($value,$id,$date);
+            $sommeachat = $em->getRepository(AchatA::class)->findBySommeAchatProduitDay($date,$value,$id);
+            $sommeventejour = $em->getRepository(FactureA::class)->findByQuantiteProduitVendu(new \DateTimeImmutable(), $value->getid(), $id);
+            $sommevente = $em->getRepository(FactureA::class)->findByQuantiteProduitVenduAnne($date, $value, $id);
+            $magasinQt = $em->getRepository(MagasinA::class)->findOneBy(['produit' => $value, 'agence' => $id]);
+            if ($historiques) {
+                $historiquequatite = $historiques->getQuantite();
+            }
+            $stocktreel = $value->getQuantite() + (empty($magasinQt)?0:$magasinQt->getQuantite());
+            array_push($produits, [$value->getNom(),$historiquequatite + $sommeachat,$sommevente,$stocktreel,$value->getPrixvente()]);
+        }
+        
+        return $this->render('stock_a/perte.html.twig', [
+            'produits' => $produit,
+            'produitsData' => $produits,
+
+        ]);
+    }
 }
