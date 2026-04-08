3eacee16e3706b121da567c9140df98a13e1d769
MarcYoba <yobamarc1@gmail.com>
2026-04-08 14:44:55 +0100

    creation des inventaires de la provenderie

---
+++ public/js/inventaire/inventair.js
@@ -0,0 +1,66 @@
+const rechercheproduit = document.getElementById("inventaire_inventaire");
+
+rechercheproduit.addEventListener('input',calculeTotal);
+
+function calculeTotal(){
+    let quantite = parseInt(document.getElementById("quantite").value) || 0 ;
+    let stock = parseInt(document.getElementById("inventaire_inventaire").value) || 0 ;
+    let contoire = parseInt(document.getElementById("stock").value) || 0 ;
+    let vendu = parseInt(document.getElementById("vendu").value) || 0 ;
+    
+    document.getElementById("inventaire_ecart").value = (stock) - (contoire + quantite );    
+}
+
+function recherchequantite(){
+    
+    let data = {};
+    const nomproduit  = document.getElementById("inventaire_produit").value ;
+    const date = document.getElementById("inventaire_createtAt").value ;
+    // collection de l'id du produit
+    data.nom = nomproduit;
+    data.date = date;
+    //console.log(nomproduit);
+
+    fetch('/produit/a/recherche/quantite',{
+        method:'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => { 
+        if (data.success == true) {
+            document.getElementById("quantite").value = data.quantite;
+            document.getElementById("inventaire_a_quantite").value = data.contoire;
+            document.getElementById("vendu").value = data.facturation;
+            document.getElementById("stock").value = data.contoire;
+            console.log(data);
+        }else if(data.success == false){
+        }else{
+            console.log(data);
+        }     
+    })
+    .catch(error => {
+        console.error(error);
+    });
+       
+}
+
+function rechercheInventaire() {
+            // Récupérer l'input et la liste déroulante
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("produitrecher");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("inventaire_a_produit");
+    li = ul.getElementsByTagName("option");       
+            // Boucler sur toutes les options
+        for (i = 0; i < li.length; i++) {
+            a = li[i];
+            if (a.textContent.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+            } else {
+            li[i].style.display = "none";
+            }
+        }           
+}
---
+++ src/Form/InventaireType.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Inventaire;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class InventaireType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('quantite',NumberType::class,[
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'definir la quantite',
+                    'readonly' => true,
+                ]
+            ])
+            ->add('ecart',NumberType::class,[
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'difference',
+                    'readonly' => true,
+                ]
+            ])
+            ->add('inventaire',NumberType::class,[
+                
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'definir l\'inventaire',
+                ]
+            ])
+            ->add('produit',EntityType::class,[
+                'class' => 'App\Entity\Produit',
+                'choice_label' => 'nom',
+                
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Sélectionner un produit',
+                ],
+            ])
+            ->add('createtAt',DateType::class,[
+                'widget' => 'single_text',
+                'attr' => [
+                    'class' => 'form-control',
+                ]
+            ])
+            ->add('justificatif',TextType::class,[
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'justificatif en cas d\'ecart',
+                ]
+            ])
+            ->add('button',SubmitType::class,[
+                'label' => 'Enregistrer',
+                'attr' => [
+                    'class' => 'btn btn-primary btn-user btn-block',
+                    'style' => 'margin-top: 1rem;' // Adds spacing to move the button to a new line
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Inventaire::class,
+        ]);
+    }
+}
